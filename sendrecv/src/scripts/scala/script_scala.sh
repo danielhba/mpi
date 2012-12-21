75acfa663f31ca2937976c80d016f7087ec378cd
Daniel Henrique Braz de Aquino <dhbaquino@gmail.com>
2012-12-21 01:31:14 -0400

    Benchmark de SendRecvAll em Java e ajeitando scripts de execução

--- sendrecv/src/scripts/scala/script_scala.sh
+++ sendrecv/src/scripts/scala/script_scala.sh
@@ -1,7 +1,7 @@
 pid=$!
 
 cd ../../scala
-scalac threadring.scala
+scalac sendrecv.scala
  
 #Inicia a Aplicacao
 for k in 1000 10000 20000 30000 50000 100000
@@ -12,7 +12,7 @@ for h in 5000 10000 50000
 do
 for i in 1 2 3 4 5 6 7 8 9 10 
 do
-scala -J-Xmx8g threadring $h $j $k sendrecv_scala_"$h"_"$j"_"$k".txt
+scala -J-Xmx8g sendrecv $h $j $k sendrecv_scala_"$h"_"$j"_"$k".txt
 echo "$h DataSize $j Repetitions $k Process - Scala"
 done
 done
--- threadring/src/scripts/erlang/script_erl.sh
+++ threadring/src/scripts/erlang/script_erl.sh
@@ -16,7 +16,7 @@ for h in 5000 10000 50000
 do
 for i in 1 2 3 4 5 6 7 8 9 10 
 do
-erl -smp enable -noshell -run threadring run $k $l $j
+erl -smp enable -noshell -eval "threadring:run([\"$h\",\"$j\",\"$k\"])." -s init stop
 echo "$h DataSize $j Repetitions $k Process - Erlang"
 done
 done
